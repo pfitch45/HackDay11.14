974ce67439e90ad962bb79f7366edb56a0f285f2
pfitch45 <pfitch45@gmail.com>
2014-11-11 13:19:00 -0800

    remove node_modules and fix server port

--- README.md
+++
@@ -1,7 +0,0 @@
-HackDay11.14
-============
-
-Hack Day project Nov 2014
-
-
-This is an attempt to make my first real web app.  Let's see if I don't fuck it up.
---
+++ server.js
@@ -0,0 +1,49 @@
+//server.js file
+
+//modules **********************************************
+var express				= require('express');
+var app					= express();
+var bodyParser 			= require('body-parser');
+var methodOverride 		= require('method-override');
+
+//configurizatsiya *********************************************
+
+var db = require('./config/db');
+
+//set port
+
+var port = process.env.PORT || 9000;
+
+//connect to MongoDB database
+//(uncomment after you enter in your own credentials in config/db.js)
+//mongoose.connect(db.url);
+
+//get all data of the body (POST) parameters
+//parse application/json
+app.use(bodyParser.json());
+
+//parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json' }));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//override with the X-HTTP-Method-Overrride header in the request.  simunlate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Overrride' ));
+
+//set the static files location/public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+//routes *********************************
+
+require('./app/routes')(app); //configure our routes
+
+//start app ***************************
+//start up at http://localhost:8080 
+app.listen(port);
+
+//shortcut to the user 
+console.log('Magic happens on port ' + port);
+
+//expose app
+exports = module.exports = app;
